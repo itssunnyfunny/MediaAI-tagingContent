dc00ec9e03048e56bf30fff6e7ddd38d349cd5f7
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-11-29 11:36:25 +0530

    fix(web): use xxhash64 for webpack hashing

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@repo/ui'],
+  webpack: (config) => {
+    config.output.hashFunction = "xxhash64";
+    return config;
+  },
 };
 
 export default nextConfig;
